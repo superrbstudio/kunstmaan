377454c29d9a3ec3384300071154695f2d4cb23e
James Dinsdale <hi@molovo.co>
2019-09-02 09:00:24 +0100

    Add an event subscriber to set Turbolinks-Location headers on responses that follow a redirect
    Because Turbolinks uses XHR requests, it only sees the final response in
    the chain, with no way of knowing that the request has followed a
    redirect. This means that the browser's address bar shows the URL that
    was requested, not the correct URL for the response that was returned.
    
    This MR adds an Event Subscriber which listens for Kernel
    responses, sets the URL in the session on redirecting, and includes this
    in the `Turbolinks-Location` header for the next response. On
    seeing this, Turbolinks will silently update the URL so that the address
    bar shows the correct URL.

---
+++ src/EventSubscriber/TurbolinksLocationSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Superrb\KunstmaanAddonsBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class TurbolinksLocationSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var string
+     */
+    const SESSION_KEY = 'turbolinks.redirect-location';
+
+    /**
+     * @var string
+     */
+    const HEADER_KEY = 'Turbolinks-Location';
+
+    /**
+     * @return string[]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.response' => ['onKernelResponse'],
+        ];
+    }
+
+    /**
+     * @param SessionInterface $session
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param ResponseEvent $event
+     */
+    public function onKernelResponse(ResponseEvent $event)
+    {
+        /** @var Response $response */
+        $response = $event->getResponse();
+
+        if ($response instanceof RedirectResponse) {
+            $this->session->set(self::SESSION_KEY, $response->getTargetUrl());
+        } elseif ($url = $this->session->get(self::SESSION_KEY)) {
+            $response->headers->set(self::HEADER_KEY, $url);
+            $this->session->set(self::SESSION_KEY, null);
+        }
+    }
+}
