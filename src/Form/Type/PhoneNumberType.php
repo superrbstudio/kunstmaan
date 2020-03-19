c784e4d77f6790dde0cd40ca7a26f5c2bb04f351
James Dinsdale <hi@molovo.co>
2020-03-19 09:39:16 +0000

    Add wrapper for phone number types with simplified country dropdown

---
+++ src/Form/Type/PhoneNumberType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Superrb\KunstmaanAddonsBundle\Form\Type;
+
+use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType as MisdPhoneNumberType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * A simple form type which extends the phone number type field, and replaces
+ * the list of countries with their ISO code for brevity.
+ */
+class PhoneNumberType extends MisdPhoneNumberType
+{
+    /**
+     * @return void
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $preferredChoices = $view->children['country']->vars['preferred_choices'];
+        $choices          = $view->children['country']->vars['choices'];
+
+        foreach ([$preferredChoices, $choices] as $items) {
+            foreach ($items as $item) {
+                $item->label = preg_replace_callback('/(?:.*)(\(\+\d+\))/', function ($matches) use ($item) {
+                    return $item->value.' '.$matches[1];
+                }, $item->label);
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults([
+            'widget'                    => MisdPhoneNumberType::WIDGET_COUNTRY_CHOICE,
+            'preferred_country_choices' => ['GB'],
+            'default_region'            => 'GB',
+        ]);
+    }
+}
